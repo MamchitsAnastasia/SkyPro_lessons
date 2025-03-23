a06c7dfbc41a66907f0db94acd2bdefb3db04403
Mamchits Anastasia <katara221995@mail.ru>
2025-03-23 23:54:21 +0700

    README file added to flake8 exceptions, "masks" and "test_demo" modules formatted..

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,6 @@
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску"""
-    mask_card_number = (
-        card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-    )
+    mask_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
     return mask_card_number
 
 
@@ -10,4 +8,3 @@ def get_mask_account(account_id: str) -> str:
     """Принимает на вход номер счета и возвращает его маску"""
     mask_account = "**" + account_id[-4:]
     return mask_account
-
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,7 +1,7 @@
 from src import widget
 from src import processing
 
-list_of_operations =[
+list_of_operations = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
