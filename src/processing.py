756f0a006d399d461708ef1929e065e0b979a944
Mamchits Anastasia <katara221995@mail.ru>
2025-03-23 18:43:43 +0700

    The "sort_by_date" function has been added.

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,9 @@ def filter_by_state (list_of_operations, state="EXECUTED"):
             filtered_list_of_operations.append(operation)
 
     return filtered_list_of_operations
+
+def sort_by_date (list_of_operations):
+    """Функция принимает список операций и возвращает отсортированный по дате список операций"""
+    sorted_list_of_operations = sorted(list_of_operations, key=lambda operation: operation["date"], reverse=True)
+
+    return sorted_list_of_operations
