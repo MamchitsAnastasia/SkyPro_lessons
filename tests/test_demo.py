aecfc801ab0af6ef273843a0e0cb8997e0e3daec
Mamchits Anastasia <katara221995@mail.ru>
2025-03-23 18:19:36 +0700

    Initializing a new branch "develop"

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -6,3 +6,5 @@ print("Номер карты с маской: ", widget.mask_account_card(accoun
 
 unformatted_date = input("Введите дату: ")
 print("Отформатированная дата: ", widget.get_date(unformatted_date))
+
+print("Тестовые изменения для инициализации новой ветки 'develop'")
