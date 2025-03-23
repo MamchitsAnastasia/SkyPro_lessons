98fceca787b6b7d28b83715d9a5d4c6c7b84237f
Mamchits Anastasia <katara221995@mail.ru>
2025-03-23 22:16:29 +0700

    Final check of functions.

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -8,11 +8,11 @@ list_of_operations =[
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-#account_card = input("Введите номер карты или счёта: ")
-#print("Номер карты с маской: ", widget.mask_account_card(account_card))
+account_card = input("Введите номер карты или счёта: ")
+print("Номер карты с маской: ", widget.mask_account_card(account_card))
 
-#unformatted_date = input("Введите дату: ")
-#print("Отформатированная дата: ", widget.get_date(unformatted_date))
+unformatted_date = input("Введите дату: ")
+print("Отформатированная дата: ", widget.get_date(unformatted_date))
 
 print(processing.filter_by_state(list_of_operations, "CANCELED"))
 print(processing.sort_by_date(list_of_operations, True))
