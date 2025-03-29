9dc73ce83c3b0256ecdc0b74c8c8f3e707854ab4
Mamchits Anastasia <katara221995@mail.ru>
2025-03-30 00:55:56 +0700

    Wrote fixtures in the "conftest" module.

---
+++ tests/conftest.py
@@ -0,0 +1,69 @@
+import pytest
+from datetime import date, timedelta
+from random import choice, randint, sample
+from string import ascii_uppercase, digits
+
+
+# Фикстура для генерации случайных номеров карт
+@pytest.fixture(scope="session")
+def generate_random_card_number():
+    def _generate(length=16):
+        return ''.join(str(randint(0, 9)) for _ in range(length))
+
+    return _generate
+
+
+# Фикстура для генерации случайных номеров счетов
+@pytest.fixture(scope="session")
+def generate_random_account_id():
+    def _generate(length=20):
+        return ''.join(choice(ascii_uppercase + digits) for _ in range(length))
+
+    return _generate
+
+
+# Фикстура для генерации случайных наименований карт/счетов
+@pytest.fixture(scope="session")
+def generate_random_account_card_name():
+    types = ["Счет", "Карта"]
+    lengths = [20, 30]
+
+    def _generate(type=None, length=None):
+        if not type:
+            type = choice(types)
+        if not length:
+            length = choice(lengths)
+
+        name = type + '-' + ''.join(sample(digits, length))
+        return name
+
+    return _generate
+
+
+# Фикстура для генерации случайных дат
+@pytest.fixture(scope="session")
+def generate_random_date():
+    today = date.today()
+    days_ago = randint(-365, 365)
+    random_date = today + timedelta(days=days_ago)
+    return random_date.strftime("%Y-%m-%d")
+
+
+# Фикстура для генерации списка операций
+@pytest.fixture(scope="session")
+def generate_operations_list():
+    states = ["EXECUTED", "PENDING", "FAILED"]
+    dates = [date.today().strftime("%Y-%m-%d")]
+
+    operations = []
+    for i in range(10):
+        operation = {
+            "id": i,
+            "name": f"Operation-{i}",
+            "date": choice(dates),
+            "state": choice(states),
+            "amount": randint(100, 20000)
+        }
+        operations.append(operation)
+
+    return operations
