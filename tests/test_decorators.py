d0447c2ee956f08663c7621be949d96366f0ef00
Mamchits Anastasia <katara221995@mail.ru>
2025-04-13 23:19:52 +0700

    The "test_decorators" module has been written.

---
+++ tests/test_decorators.py
@@ -0,0 +1,58 @@
+import pytest
+from src import decorators
+
+
+@decorators.log()
+def divide(a, b):
+    """Функция получает два аргумента int и выполняет деление первого аргумента на второй"""
+    return a / b
+
+def test_log_decorator_stdout_success(capsys):
+    """Тестирует декоратор log на функции без ошибки, при выводе логов в консоль"""
+    result = divide(6, 3)
+    captured = capsys.readouterr()
+    assert "divide started. Inputs: (6, 3), {}" in captured.out
+    assert "divide ok" in captured.out
+    assert result == 2
+
+
+def test_log_decorator_stdout_error(capsys):
+    """Тестирует декоратор log на функции с ошибкой ZeroDivisionError - деления на 0, при выводе логов в консоль"""
+    with pytest.raises(ZeroDivisionError):
+        divide(1, 0)
+    captured = capsys.readouterr()
+    assert "divide started. Inputs: (1, 0), {}" in captured.out
+    assert "divide error: ZeroDivisionError. Inputs: (1, 0), {}" in captured.out
+
+
+def test_log_decorator_file_success(tmp_path):
+    """Тестирует декоратор log на функции без ошибки, при выводе логов в текстовый файл"""
+    log_file = tmp_path / "test.log" #Создание временного файля для логов
+
+    @decorators.log(filename=str(log_file))
+    def multiply(a, b):
+        """Функция получает два аргумента int и выполняет умножение первого аргумента на второй"""
+        return a * b
+
+    result = multiply(3, 4)
+    log_content = log_file.read_text() #Чтение содержимого временного файла
+    assert result == 12
+    assert "multiply started. Inputs: (3, 4), {}" in log_content
+    assert "multiply ok" in log_content
+
+
+def test_log_decorator_file_error(tmp_path):
+    """Тестирует декоратор log на функции с ошибкой TypeError, при выводе логов в текстовый файл"""
+    log_file = tmp_path / "error.log" #Создание временного файля для логов
+
+    @decorators.log(filename=str(log_file))
+    def faulty_func():
+        """Функция вызывает ошибку TypeError"""
+        raise TypeError("Something went wrong")
+
+    with pytest.raises(TypeError):
+        faulty_func()
+    log_content = log_file.read_text() #Чтение содержимого временного файла
+    assert "faulty_func started. Inputs: (), {}" in log_content
+    assert "faulty_func error: TypeError" in log_content
+
