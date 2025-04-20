c3050c3cdaa831a418592c29a7ed48270a866ac7
Mamchits Anastasia <katara221995@mail.ru>
2025-04-21 01:44:51 +0700

    Error handling has been added to the "external_api" module.

--- src/external_api.py
+++ src/external_api.py
@@ -12,11 +12,11 @@ BASE_URL = "https://api.apilayer.com/exchangerates_data/latest"
 def convert_to_rub(transaction: dict) -> float:
     """Функция переводит валюту в RUB, запрашивая курсы на стороннем ресурсе."""
     operation_amount = transaction.get("operationAmount", {})
-    amount = operation_amount.get("amount", 0)
+    amount = float(operation_amount.get("amount", 0))
     currency = operation_amount.get("currency", {}).get("code", "RUB").upper()
 
     if currency == "RUB":
-        return float(amount)
+        return amount
 
     try:
         response = requests.get(
@@ -26,9 +26,6 @@ def convert_to_rub(transaction: dict) -> float:
         rates = response.json().get("rates", {})
         rate = rates.get("RUB", 1)
         return float(amount) * float(rate)
-    except (requests.RequestException, ValueError, KeyError):
-        # Ошибка requests.RequestException если возникла проблема при запросе к API
-        # (нет интернета, сервер не отвечает и т.п.)
-        # Ошибка ValueError если данные от API пришли в неверном формате (например, вместо числа пришла строка)
-        # Ошибка KeyError если в ответе API отсутствует нужное поле (например, нет ключа "rates")
-        return 0.0
+
+    except Exception as e:
+        raise ValueError(f"Ошибка конвертации валюты: {e}")
